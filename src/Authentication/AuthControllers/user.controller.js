8257617f3d77e392477367e84965e0ffffcb2743
Darshika-ctinfotech <darshikakhillare.github@gmail.com>
2026-07-06 18:51:25 +0530

    signup/login basic

--- src/Authentication/AuthControllers/user.controller.js
+++ src/Authentication/AuthControllers/user.controller.js
@@ -1,6 +1,68 @@
-export const signup = async(req,res) => {
+// export const signup = async(req,res) => {
 
-}
-export async function signup(req,res) {
+// }
+// export async function signup(req,res) {
     
+// }
+
+import { createUser, getUser } from '../AuthModules/user.module.js';
+
+export async function signup(req,res) {
+    try{
+    const {userName, email, password, contact } = req.body;
+    const user = await createUser(userName, email, password, contact);
+
+    return res.status(200).json({
+        success: true,
+        message: "User Created Successfully",
+        user: {
+            id:         "         "+user.id,
+            userName:   "   "+user.userName,
+            email:      "      "+user.email,
+            contact:    "    "+user.contact    
+        }
+    })
+}
+    catch(err){
+        console.error("Error => ", err);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+            error: err.message
+        })
+    }
+}
+
+
+export async function login(req,res) {
+    try{
+        const { email, password } = req.body;
+        const user = await getUser(email, password)
+        if(!user){
+            return res.status(200).json({
+            success:false,
+            message: "Invalid Email or Password"
+        })
+    }
+        else{
+        return res.status(200).json({
+            success:true,
+            message: "User Logged in Successfully",
+             user: {
+            id:         "         "+user.id,
+            userName:   "   "+user.userName,
+            email:      "      "+user.email,
+            contact:    "    "+user.contact    
+        }
+        })
+    }
+}
+    catch(err){
+           console.error("Error => ", err);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error",
+            error: err.message
+        }) 
+    }
 }
---
+++ src/Middleware/uservalidator.middelware.js
@@ -0,0 +1,20 @@
+// middleware/validate.js
+import  { validationResult } from "express-validator";
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      success: false,
+      message: "Validation failed",
+      errors: errors.array().map((error) => ({
+        field: error.path,
+        message: error.msg,
+      })),
+    });
+  }
+
+  next();
+};
+
+export default validate;
